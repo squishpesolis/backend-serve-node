b1385cab16f4c4f83349afceb9e7ee05a57acbd5
StalynQuishpe <stalin.ct@hotmail.com>
2020-07-16 13:35:17 -0500

    BackEnd anterior"

--- routes/usuario.js
+++ routes/usuario.js
@@ -17,7 +17,7 @@ app.get('/', (req, res, next) => {
     var desde = req.query.desde || 0;
     desde = Number(desde);
 
-    Usuario.find({}, 'nombre email img role')
+    Usuario.find({}, 'nombre email img role google')
         .skip(desde)
         .limit(5)
         .exec(
